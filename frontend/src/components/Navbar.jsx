de7eabf37c06bb31bca6fdf0d6c231b3c9c568b6
aglucazgeorgeann-ship-it <aglucazgeorgeann@gmail.com>
2025-08-16 23:15:03 +0800

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useCart } from '../context/CartContext';
+import { useCart } from '../context/CartContext'; // I-import ang useCart hook
 
 function Navbar() {
-  const { getTotalItems } = useCart();
+  const { getTotalItems } = useCart(); // Gamitin ang getTotalItems function mula sa context
 
   return (
     <nav className="flex justify-between items-center p-4 bg-gray-800 text-white shadow-md font-sans rounded-lg m-2">
@@ -18,8 +18,8 @@ function Navbar() {
           Admin
         </Link> {/* Idinagdag mo ito */}
         <Link to="/cart" className="text-white ml-6 text-lg no-underline relative hover:text-gray-300 transition duration-300">
-          Cart ({getTotalItems()})
-          {getTotalItems() > 0 && (
+          Cart ({getTotalItems()}) {/* Ipakita ang total items sa cart */}
+          {getTotalItems() > 0 && ( // Magpakita ng badge kung may items sa cart
             <span className="absolute -top-2 -right-2 bg-red-500 rounded-full px-2 py-0.5 text-xs text-white">
               {getTotalItems()}
             </span>
